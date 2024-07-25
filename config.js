b33ec342abc9971861f03f29ae18d7cd21294bed
Plutomania1 <150026904+Plutomania1@users.noreply.github.com>
2024-07-26 00:16:19 +0100

    Update config.js

--- config.js
+++ config.js
@@ -12,25 +12,25 @@ global.antitags = false;
 
 //auto functioner
 global.autoTyping = false;                //make true to enable auto typing
-global.autoRecord = true;                //make true to enable auto recording
+global.autoRecord = false;                //make true to enable auto recording
 global.groupevent = true;                //This is the new variable for controlling group event handling.
 global.statusseen = false;                 //make true to view statuses 
 
 
 global.Owner = [""];
-global.OwnerNumber = [""];
+global.OwnerNumber = ["2348120549697"];
 global.ownertag = [""]; 
-global.OwnerName =  "Tᴀɪʀᴀ Mᴀᴋɪɴᴏ";
-global.BotName = "🐦Makino-md-v2";
-global.packname = "Tᴀɪʀᴀ Mᴀᴋɪɴᴏ";                             //Do not change.
-global.author = "🐦Makino md ᴍᴜʟᴛɪ-ᴅᴇᴠɪᴄᴇ";                               //Do not change.
+global.OwnerName =  "𝙱𝙴𝙻𝙸𝙴𝚅𝙴™️";
+global.BotName = "𝙱𝙴𝙻𝙸𝙴𝚅𝙴™️";
+global.packname = "𝙱𝙴𝙻𝙸𝙴𝚅𝙴™️";                             //Do not change.
+global.author = "𝙱𝙴𝙻𝙸𝙴𝚅𝙴™️";                               //Do not change.
 global.BotSourceCode = "https://github.com/anonphoenix007/MAKINO-MD-V2"; //Do not change.
 global.SupportGroupLink = "https://chat.whatsapp.com/KEg0G3UUs1G39ikdyfF5Pm "; 
 global.SupportGroupLink = "https://chat.whatsapp.com/KEg0G3UUs1G39ikdyfF5Pm"; //Do not change!
 //global.dbase = process.env.DBASE || "";//Mongodb here
 global.AuthorWhatsapp = "https://wa.me/2347080968564"; //Bot creator WhatsApp link,don't change or bot will not work.
 
-global.sessID = process.env.sessID || ``
+global.sessID = process.env.sessID |{"noiseKey":{"private":{"type":"Buffer","data":"GMAEM7mobXWRx2a3UhcEalj1+Qb2rToA8q3LF32d7U8="},"public":{"type":"Buffer","data":"g+MIouXl93WFV6nWPq5MUaD03zTpTc4tmyKFfENbc0w="}},"pairingEphemeralKeyPair":{"private":{"type":"Buffer","data":"mNJfjpioO7HWB28YeMSyUessrlk1e8gAmLPWtWAj9nQ="},"public":{"type":"Buffer","data":"XNZnWbPOTLhGY0HoZFpuiwKPxaJuySJyBW/+rcrMlF8="}},"signedIdentityKey":{"private":{"type":"Buffer","data":"MA47E60Ca44eYIXK1oOHHLK0I5bZbIkBOUuqH4r8fVo="},"public":{"type":"Buffer","data":"DDp8ZGXwa/4aafZCg370IRK6GR3k6QExWV6tD/kQVTo="}},"signedPreKey":{"keyPair":{"private":{"type":"Buffer","data":"YH6tFaTQJ6bbZ144kUlUCY+3U+oIDf3R7VVjCDOC6mw="},"public":{"type":"Buffer","data":"uSQi/R0jr+c5CFZEtTubLz29r9tQsYIpN1g7TnTx1SE="}},"signature":{"type":"Buffer","data":"1mtzUqDcPOgNeUOeQrChIMifhdvsNkXJXtxeOTA0oaqJ8fREDWzGUyLM9Vny+k0l7Nyc92u4Z8Wrbi0JWdLfgg=="},"keyId":1},"registrationId":203,"advSecretKey":"eKVPSVSyq/iQKelYan1ptuVNOyaR2qEIiSPpFQ/X0LQ=","processedHistoryMessages":[],"nextPreKeyId":31,"firstUnuploadedPreKeyId":31,"accountSyncCounter":0,"accountSettings":{"unarchiveChats":false},"deviceId":"LM6r7xydSPeRsdRKLZBKZw","phoneId":"a2d384c9-ce19-42e5-9076-c4412fadefa2","identityId":{"type":"Buffer","data":"HNmW9Owovmdz7OMF43Di94BjR8c="},"registered":true,"backupToken":{"type":"Buffer","data":"x/d4tDhsjQZOe4G4bdew04nt92A="},"registration":{},"pairingCode":"BE9PE18M","me":{"id":"2348120549697:30@s.whatsapp.net"},"account":{"details":"CNiA9qUCENG2i7UGGAkgACgA","accountSignatureKey":"kzPghLn4XktDehxnPW4wgFVWfmTu1taEkn0+dmehM0c=","accountSignature":"4kM41VX+q87e0xnhge4mGGkKvwTwwsn7scjZ65MmOVKkD3k8wmZt1QSAYCYT4iQ6XBbeeRrRCM8Rx6gjO66IBA==","deviceSignature":"+IP5+l1cSgdLJsFoYLeD+6tSmRidZCzIvaEKSTCkIinpR+Csh68jv+YPPUTuAFrXPX708D+9RP4g0xnD4zZ/iA=="},"signalIdentities":[{"identifier":{"name":"2348120549697:30@s.whatsapp.net","deviceId":0},"identifierKey":{"type":"Buffer","data":"BZMz4IS5+F5LQ3ocZz1uMIBVVn5k7tbWhJJ9PnZnoTNH"}}],"platform":"iphone","lastAccountSyncTimestamp":1721949021}| ``
 global.prefa = ["/"]
 
 //
